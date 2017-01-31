e08d3076d03128aefb65c083ef6dbd17b72cce6b
boys <boys@boys-ThinkPad-X201.(none)>
2017-01-31 17:21:27 +0200

    in database error exit

--- installWP.py
+++ installWP.py
@@ -28,20 +28,43 @@ else:
 	
 for x in range(5):
 	
-	subprocess.call(init.tarcmd.split(' '))
-	subprocess.call(chown_cmd.split(' '))
-	subprocess.call(chmod_cmd.split(' '))
+	if (subprocess.call(init.tarcmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in tar command"
+		exit(0)
+		
+	if (subprocess.call(chown_cmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in chown command"
+		exit(0)
+	if (subprocess.call(chmod_cmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in chmod command"
+		exit(0)
 	mv_cmd=init.mv_CMD+str(x)
 	
-	subprocess.call(mv_cmd.split(' '))
-	
+	if (subprocess.call(mv_cmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in mv command"
+		exit(0)
+		
 	mysql_cmd=init.mysql_CMD
 	print mysql_cmd
 	mysql_cmd=mysql_cmd+str(x)+mysql_user+str(x)+mysql_userAppend+str(x)+mysql_grant+str(x)+mysql_grantAppend+str(x)+"'@'localhost'\""
 	print mysql_cmd
 	p=subprocess.Popen(mysql_cmd,stdout=subprocess.PIPE,shell=True)
 
+	
 	p.communicate("exit")
+	if p.returncode==0:
+		print "Ok"
+	else:
+		print "database error"
+		exit(0)
 	mysql_cmd=""
 	
 	
