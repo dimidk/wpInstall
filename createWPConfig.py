cfe59cd31b699adffab2a0c38ec24aa9af99af5e
boys <boys@boys-ThinkPad-X201.(none)>
2017-02-07 19:41:53 +0200

    change wpconfig.php

--- createWPConfig.py
+++ createWPConfig.py
@@ -10,47 +10,92 @@ import codecs
 
 """read sample config file, each line write to config file and rewrite only
 the ones which are related to database settings. Then copy the config file 
-to the /var/www/ directory  IBAN: GR02 0172 0360 0050 3605 5747 230"""
+to the /var/www/ directory """
+
+
+AUTH_KEY="define('AUTH_KEY',         '#&y$@^N-uR2m:,}~5y><J?c&fyNk0+$Lk(Qz|l:l|m/fr| YR84Kb7pzf@4aC`Cl');"
+SECURE_AUTH_KEY="define('SECURE_AUTH_KEY',  'o2B:%2_D-5+>}r$)f8-nr<m]^wmbf/6=_m]wR*.5sH6^U{GaUgr~PG`j%1<7o]Mt');"
+LOGGED_IN_KEY="define('LOGGED_IN_KEY',    '>L_!J+>TBnCi)y]Z.M-v4.t|HEl/!yj903]2sa(F~JwZ8$cTK]Y`~t{-z5+#?tP>');"
+NONCE_KEY="define('NONCE_KEY',        '02U7xl&P&7BF<.bzp@S{j7VEL%e^<[YGPU?X<~XH/sH-^Zcik_A_wD%wukyahg2x');"
+AUTH_SALT="define('AUTH_SALT',        'E6yiwA51W^$nb([FMb:8+btm_.i--J(.>eMrjOb)3#n43^5!#Lr4]g`+-x(4b6;k');"
+SECURE_AUTH_SALT="define('SECURE_AUTH_SALT', 'aikPT]:q@UCC.1-9BIk-?QdCHyO||IwP)B9hM2Vk!2p{?`(* >qk!xs0$/~p5_I;');"
+LOGGED_IN_SALT="define('LOGGED_IN_SALT',   '5eE(,a&MG.Vvr|Ne3HBg*7V(l4NpWDaOlZdZffex+,`}Ei,]s3h3EWV)&I0Whk|[');"
+NONCE_SALT="define('NONCE_SALT',       '%~K7hi>Fs#+6*]+ef2zj}VV$v-l3PYF>P}>93H~v0;~Sl~o4-{Ipe9X|Q;>Zj#p-');"
+
+
+
+""" add the above to file to continue with installation"""
 
 DBNAME="DB_NAME"
 DBUSER="DB_USER"
 DBPASSWD="DB_PASSWORD"
+DB_COLLATE="DB_COLLATE"
 
 def wpConfig(curWPDir,homeDir,x):
 	
 	readFileHandler=codecs.open(curWPDir+"/"+init.sampleConfigFile,"r")
+	
 	currentDir=os.getcwd()
 	os.chdir(homeDir)
 	currentDir=os.getcwd()
 	print currentDir
 	writeFileHandler=codecs.open(homeDir+"/"+init.configFile,'w')
-	"""writeFileHandler=open(curWPDir+"/"+init.configFile,"w+")"""
-	
 	
 	line=readFileHandler.readline()
 
 	while True:
-		print line
 		
-		if line.find(DBNAME):
+		if line.find(DBNAME)!=-1:
+			print "I am in dbname"
 			writeLine="define('"+DBNAME+"', '"+init.baseDir+"wp"+str(x)+"')"
-			writeFileHandler.write(writeLine+"\n")
+			writeFileHandler.write(writeLine)
 			print writeLine
 			
-		elif line.find(DBUSER):
+		elif line.find(DBUSER)!=-1:
+			print "I am in dbuser"
 			writeLine="define('"+DBUSER+"', '"+init.baseDir+"wp"+str(x)+"')"
-			writeFileHandler.write(writeLine+"\n")
+			writeFileHandler.write(writeLine)
 			print writeLine
 			
-		elif line.find(DBPASSWD):
+		elif line.find(DBPASSWD)!=-1:
+			print "I am in dbpasswd"
 			writeLine="define('"+DBPASSWD+"', '"+init.baseDir+"wp"+str(x)+"')"
-			writeFileHandler.write(writeLine+"\n")
+			writeFileHandler.write(writeLine)
 			print writeLine
+		
+		elif line.find(DB_COLLATE)!=-1:
+			print "I am in dbcollate"
+			writeFileHandler.write(line)
+		elif line.find('AUTH_KEY')!=-1:
+
+			writeFileHandler.write(AUTH_KEY+"\n")
+		elif line.find('SECURE_AUTH_KEY')!=-1:
+			
+			writeFileHandler.write(SECURE_AUTH_KEY+"\n")
+		elif line.find('LOGGED_IN_KEY')!=-1:
+			
+			writeFileHandler.write(LOGGED_IN_KEY+"\n")
+		elif line.find('NONCE_KEY')!=-1:
+			
+			writeFileHandler.write(NONCE_KEY+"\n")
+		elif line.find('AUTH_SALT')!=-1:
+			
+			writeFileHandler.write(AUTH_SALT+"\n")
+		elif line.find('SECURE_AUTH_SALT')!=-1:	
+			
+			writeFileHandler.write(SECURE_AUTH_SALT+"\n")
+		elif line.find('LOGGED_IN_SALT')!=-1:
+			
+			writeFileHandler.write(LOGGED_IN_SALT+"\n")
+		elif line.find('NONCE_SALT')!=-1:
+			
+			writeFileHandler.write(NONCE_SALT+"\n")
 			
 		else:
 			print line
-			writeFileHandler.write(line+"\n")
+			writeFileHandler.write(line)
 			print line
+			
 			
 		line=readFileHandler.readline()
 
--- installWP.py
+++ installWP.py
@@ -13,10 +13,17 @@ mysql_userAppend="'@'localhost' identified by '"+init.baseDir+"wp"
 mysql_grant="'; GRANT ALL PRIVILEGES ON "+init.baseDir+"wp"
 mysql_grantAppend=".* to '"+init.baseDir+"wp"
 
-
-
 chown_cmd=init.chown_CMD+"/wordpress"
 chmod_cmd=init.chmod_CMD+"/wordpress"
+
+def callCommand(cmd):
+	
+	if (subprocess.call(cmd))==0:
+		print "Ok"
+	else:
+		print "Error in",cmd," command"
+		exit(0)
+		
 
 curDir=os.getcwd()
 homeDir=curDir
@@ -28,9 +35,9 @@ else:
 	print "You are in wrong directory"
 	exit(0)
 	
-for x in range(1):
+for x in range(2):
 	
-	if (subprocess.call(init.tarcmd.split(' ')))==0:
+	"""if (subprocess.call(init.tarcmd.split(' ')))==0:
 		print "Ok"
 	else:
 		print "error in tar command"
@@ -45,23 +52,21 @@ for x in range(1):
 		print "Ok"
 	else:
 		print "error in chmod command"
-		exit(0)
+		exit(0)"""
+		
+	callCommand(init.tarcmd.split(' '))
+	callCommand(chown_cmd.split(' '))
+	callCommand(chmod_cmd.split(' '))
+	
 	mv_cmd=init.mv_CMD+str(x)
 	
-	if (subprocess.call(mv_cmd.split(' ')))==0:
+	"""if (subprocess.call(mv_cmd.split(' ')))==0:
 		print "Ok"
 	else:
 		print "error in mv command"
-		exit(0)
-		
-	curWPDir=init.installDir+"/wp"+str(x)
-	"""mv_file="sudo mv "+curWPDir+"/"+init.sampleConfigFile+" "+curWPDir+"/"+init.configFile
-	
-	if (subprocess.call(mv_file.split(' ')))==0:
-		print "Ok"
-	else:
-		print "error in renaming file"
 		exit(0)"""
+	callCommand(mv_cmd.split(' '))
+		
 	
 	
 	mysql_cmd=init.mysql_CMD
@@ -78,9 +83,48 @@ for x in range(1):
 		print "database error"
 		exit(0)
 	
-	
-	createWPConfig.wpConfig(curWPDir,homeDir,x)
 	mysql_cmd=""
+	
+for x in range(2):
+	
+	curWPDir=init.installDir+"/wp"+str(x)
+	createWPConfig.wpConfig(curWPDir,homeDir,x)
+	
+	
+	cp_cmd="sudo cp "+init.configFile+" "+init.installDir+"/wp"+str(x)
+	mvfile_cmd="sudo cp " + init.installDir+"/wp" + str(x)+"/"+init.configFile +" "+init.installDir+"/wp" + str(x)+"/"+init.sampleConfigFile
+	callCommand(cp_cmd.split(' '))
+	"""if (subprocess.call(cp_cmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in renaming file"
+		exit(0)"""
+	
+	curDir=os.getcwd()
+	print curDir
+	
+	chown_cmd=init.chown_CMD+"/wp"+str(x)
+	chmod_cmd=init.chmod_CMD+"/wp"+str(x)
+	
+	callCommand(chown_cmd.split(' '))
+	"""callCommand(chmod_cmd.split(' '))"""
+	callCommand(mvfile_cmd.split(' '))
+	
+	cp_cmd="sudo cp "+init.installDir+"/wp"+str(x)+"/"+init.sampleConfigFile+" "+init.installDir+"/wp"+str(x)+"/"+init.configFile
+	
+	"""if (subprocess.call(chown_cmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in chown command"
+		exit(0)
+		
+	if (subprocess.call(chmod_cmd.split(' ')))==0:
+		print "Ok"
+	else:
+		print "error in chmod command"
+		exit(0)"""
+		
+	
 	
 	
 	
